f1a661987109260d9aedcb59583bca919ecd2620
DejanCiev <deni_ciev@yahoo.com>
2016-04-11 09:08:09 +0200

    Change domain url address

--- src/GeoMegasoftMobile/www/js/app.js
+++ src/GeoMegasoftMobile/www/js/app.js
@@ -172,19 +172,19 @@
 
 })();
 
-(function() {
-    'use strict';
-  angular.module('starter.constants', [])
-    .constant('WebAPIurl', 'http://localhost:16952/')
-    ;
-})();
-
-//(function () {
+//(function() {
 //    'use strict';
-//    angular.module('starter.constants', [])
-//      .constant('WebAPIurl', 'http://geomegasoft.dev.haselt.net/')
+//  angular.module('starter.constants', [])
+//    .constant('WebAPIurl', 'http://localhost:16952/')
 //    ;
 //})();
+
+(function () {
+    'use strict';
+    angular.module('starter.constants', [])
+      .constant('WebAPIurl', 'http://geomegasoft.dev.haselt.net/')
+    ;
+})();
 
 
 (function() {
